c3bc194f50360ae62bbcf50f27988c0ce6f3e6f1
Clay Kuznia <clay-kuznia@hotmail.com>
2013-12-07 15:50:50 -0700

    Test change

--- Stevie/src/org/bullbots/stevie/Robot.java
+++ Stevie/src/org/bullbots/stevie/Robot.java
@@ -25,6 +25,8 @@ public class Robot extends IterativeRobot {
     private static JoystickController rightJoystick;
     private DriveTrain driveTrain;
     
+    // Edit 1
+    
     private final double P = 0.0;
     private final double I = 0.0;
     private final double D = 0.0;
